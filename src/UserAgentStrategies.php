1088db9a0a11fa9517a2d33cf28cb70340f6775d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 00:30:40 +0100

    UserAgentStrategies constants now point to classes

--- src/UserAgentStrategies.php
+++ src/UserAgentStrategies.php
@@ -2,9 +2,11 @@
 
 namespace ApiClients\Foundation\Transport;
 
+use ApiClients\Foundation\Transport\UserAgentStrategy\PackageVersionStrategy;
+use ApiClients\Foundation\Transport\UserAgentStrategy\StringStrategy;
+
 final class UserAgentStrategies
 {
-    const STRING                    = 'string';
-    const PACKAGE_VERSION           = 'package_version';
-    const PACKAGE_VERSION_PLUS_HASH = 'package_version_plus_hash';
+    const STRING                    = StringStrategy::class;
+    const PACKAGE_VERSION           = PackageVersionStrategy::class;
 }
