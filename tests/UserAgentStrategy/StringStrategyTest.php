b3bb9ee8977a46e6d3961c5f385f7e5f04c2a195
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-20 00:37:51 +0100

    String strategy

---
+++ tests/UserAgentStrategy/StringStrategyTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace ApiClients\Foundation\Transport\UserAgentStrategy;
+
+use ApiClients\Foundation\Transport\Options;
+use ApiClients\Tools\TestUtilities\TestCase;
+use InvalidArgumentException;
+
+final class StringStrategyTest extends TestCase
+{
+    public function testWorking()
+    {
+        $userAgent = 'abc';
+
+        $this->assertSame(
+            $userAgent,
+            (new StringStrategy())->determineUserAgent([
+                Options::USER_AGENT => $userAgent,
+            ])
+        );
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Missing user agent option
+     */
+    public function testFail()
+    {
+        (new StringStrategy())->determineUserAgent([]);
+    }
+}
