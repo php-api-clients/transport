864c0c9cb214ff2d24ff5ba9443de6354d372adc
Jaapio <jaap@ijaap.nl>
2017-01-25 20:45:08 +0100

    Implement usage of middleware locator
    Removes the usage of ContainerInterface in Client and adds the
    usage of Locator to fetch middlewares.

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -3,11 +3,14 @@
 namespace ApiClients\Tests\Foundation\Transport;
 
 use ApiClients\Foundation\Events\CommandLocatorEvent;
+use ApiClients\Foundation\Middleware\Locator\ContainerLocator;
+use ApiClients\Foundation\Middleware\Locator\Locator;
 use ApiClients\Foundation\Transport\Client;
 use ApiClients\Foundation\Transport\Factory;
 use ApiClients\Foundation\Transport\Options;
 use ApiClients\Tools\TestUtilities\TestCase;
 use DI\ContainerBuilder;
+use Interop\Container\ContainerInterface;
 use League\Event\Emitter;
 use League\Event\EmitterInterface;
 use React\EventLoop\Factory as LoopFactory;
@@ -17,6 +20,7 @@ class FactoryTest extends TestCase
     public function testCreate()
     {
         $container = ContainerBuilder::buildDevContainer();
+        $container->set(Locator::class, new ContainerLocator($container));
         $loop = LoopFactory::create();
         $client = Factory::create(
             $container,
